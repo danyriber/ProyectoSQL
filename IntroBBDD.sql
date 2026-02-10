8ecc38e5f594cfc2181fe855ca3760e5b33bf4b4
danyriber <danyriber3232@gmail.com>
2026-02-10 21:40:54 +0100

    otros 2 pa la saca

--- IntroBBDD.sql
+++ IntroBBDD.sql
@@ -8,12 +8,18 @@ WHERE status = 'On Time';
 2. Escribe una consulta que extraiga todas las columnas de la tabla bookings y refleje todas las reservas
  que han supuesto una cantidad total mayor a 1.000.000 (Unidades monetarias).
 
- SELECT *
-FROM bookings.bookings
+ SELECT * FROM bookings.bookings
 WHERE total_amount > 1000000;
 
-1. Escribe una consulta que recupere los Vuelos (flights) y su identificador que figuren con status On Time.
+3. Escribe una consulta que extraiga todas las columnas de los datos de los modelos de aviones disponibles (aircraft_data).
+ Puede que os aparezca en alguna actualización como "aircrafts_data", revisad las tablas y elegid la que corresponda.
 
-SELECT flight_id, status
+ SELECT *
+FROM bookings.aircrafts_data;
+
+4. Con el resultado anterior visualizado previamente, escribe una consulta que extraiga los identificadores 
+de vuelo que han volado con un Boeing 737. (Código Modelo Avión = 733)
+
+SELECT flight_id
 FROM bookings.flights
-WHERE status = 'On Time';
+WHERE aircraft_code = '733';
